bee0d011e946aaaf9653e4b6b3244405e1d716b9
Josh Gillette <joshtgillette@gmail.com>
2020-12-21 09:06:27 -0500

    Remove unecessary 'id' tags and parameters

--- meta/forms.py
+++ meta/forms.py
@@ -4,40 +4,29 @@ from django import forms
 class CreateAccountForm(forms.Form):
 
     firstName = forms.CharField(label='',
-                                widget=forms.TextInput(attrs={'id': 'fullNameInput',
-                                                              'placeholder': 'First name',
-                                                              'autocomplete': 'off'}),
+                                widget=forms.TextInput(attrs={'placeholder': 'First name', 'autocomplete': 'off'}),
                                 max_length=100)
 
     lastName = forms.CharField(label='',
-                               widget=forms.TextInput(attrs={'id': 'lastNameInput',
-                                                             'placeholder': 'Last name',
-                                                             'autocomplete': 'off'}),
+                               widget=forms.TextInput(attrs={'placeholder': 'Last name', 'autocomplete': 'off'}),
                                max_length=100)
 
     email = forms.CharField(label='',
-                            widget=forms.TextInput(attrs={'id': 'emailInput',
-                                                          'placeholder': 'Email',
-                                                          'autocomplete': 'off'}),
+                            widget=forms.TextInput(attrs={'placeholder': 'Email', 'autocomplete': 'off'}),
                             max_length=100)
 
     password = forms.CharField(label='',
-                               widget=forms.PasswordInput(attrs={'id': 'passwordInput',
-                                                                 'placeholder': 'Password'}))
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     repeatPassword = forms.CharField(label='',
-                                     widget=forms.PasswordInput(attrs={'id': 'repeatPasswordInput',
-                                                                       'placeholder': 'Repeat password'}))
+                                     widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password'}))
 
 
 class LoginForm(forms.Form):
 
     email = forms.CharField(label='',
-                            widget=forms.TextInput(attrs={'id': 'emailInput',
-                                                          'placeholder': 'Email',
-                                                          'autocomplete': 'off'}),
+                            widget=forms.TextInput(attrs={'placeholder': 'Email', 'autocomplete': 'off'}),
                             max_length=100)
 
     password = forms.CharField(label='',
-                               widget=forms.PasswordInput(attrs={'id': 'passwordInput',
-                                                                 'placeholder': 'Password'}))
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
