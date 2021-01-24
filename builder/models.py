40c624186d17735387708501f8db179a58cf9844
Josh Gillette <joshtgillette@gmail.com>
2021-01-23 23:27:52 -0500

    Create CreatorUser object

--- builder/models.py
+++ builder/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 
 LENGTH_SHORT = 30
+LENGTH_MEDIUM = 100
 
 
 class Page(models.Model):
     name = models.CharField(max_length=LENGTH_SHORT)
     iconFileName = models.CharField(max_length=LENGTH_SHORT)
+
+
+class CreatorUser(models.Model):
+    organization = models.CharField(max_length=LENGTH_MEDIUM)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
